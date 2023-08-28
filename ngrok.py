37a621c9bc6023f6db41d679887d77b3f80d0c60
Hans clisman Saldias Mura <126526223+HansSaldias@users.noreply.github.com>
2023-08-28 09:51:58 -0400

    Add files via upload

---
+++ ngrok.py
@@ -0,0 +1,49 @@
+
+import os  
+from time import  sleep
+logo = """
+'##::: ##::'######:::'########:::'#######::'##:::'# #:
+ ###:: ##:'##... ##:: ##.... ##:'##.... ##: ##::'##::
+ ####: ##: ##:::..::: ##:::: ##: ##:::: ##: ##:'##:::
+ ## ## ##: ##::'####: ########:: ##:::: ##: #####::::
+ ##. ####: ##::: ##:: ##.. ##::: ##:::: ##: ##. ##:::
+ ##:. ###: ##::: ##:: ##::. ##:: ##:::: ##: ##:. ##::
+ ##::. ##:. ######::: ##:::. ##:. #######:: ##::. ##:
+..::::..:::......::::..::::::..:::.......:::..::::. .::
+    \n"""
+print(logo)    
+presentation = """
+     Created  by:  1LugarParaProgramar
+
+     Author:    Hans  Saldias
+     
+     Script : Ngrok Intalacion 
+     
+     Fecha :  Viernes 25 de agosto \n\n"""
+
+for i in presentation:
+     print(i, end="", flush=True)
+     sleep(0.1)
+
+
+Info = """
+NGROK es una herramienta de uso gratuito     que nos permite exponer nuestro entorno local a la web, es decir, podemos "publicar" nuestro trabajo en local para que el resto del mundo lo pueda ver sin la necesidad de subir la aplicación a un servidor.\n\n""" 
+
+for i in Info:
+     print(i, end="", flush=True)
+     sleep(0.1)
+     
+     
+p = input("Desea instalar Ngrok (y/n) => ")      
+if p == "y":    
+    install = """curl -s https://ngrok-agent.s3.            amazonaws.com/ngrok.asc | sudo tee /etc/apt/       trusted.gpg.d/ngrok.asc >/dev/null && echo     "deb https://ngrok-agent.s3.amazonaws.com     buster main" | sudo tee /etc/apt/sources.list.d/    ngrok.list && sudo apt update && sudo apt             install ngrok"""
+    os.system(install)
+    authtoken = input("Ingrese authtoken =>  ")
+
+    ad_token = "ngrok authtoken " +         authtoken
+
+    port = input("Ingrese  puerto ejemplo (80) => ")
+    start_tunel = "ngrok http " + port
+else:
+    print("1LugarParaProgramar {°-°}")
+ 
